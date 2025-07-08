b5e8437f3647b4201018e0631d59b5de76efd526
blendonl <blendonluta@gmail.com>
2025-07-08 13:34:45 +0000

    add entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,6 +3,8 @@ class Entry < ApplicationRecord
 
   monetize :amount
 
+  entries :entry, optional: true
+
   belongs_to :account
   belongs_to :transfer, optional: true
   belongs_to :import, optional: true
