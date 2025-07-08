371988f00833cc5bc3a088ba927deb32cc4820b8
blendonl <blendonluta@gmail.com>
2025-07-08 13:56:21 +0000

    make parent entry nullabl

--- db/migrate/20250708114932_add_parent_entry_to_entries.rb
+++ db/migrate/20250708114932_add_parent_entry_to_entries.rb
@@ -1,5 +1,5 @@
 class AddParentEntryToEntries < ActiveRecord::Migration[7.2]
   def change
-    add_reference :entries, :parent_entry, null: false, foreign_key: true, type: :uuid
+    add_reference :entries, :parent_entry, null: true, foreign_key: true, type: :uuid
   end
 end
