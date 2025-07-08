bec269092eb9ba5699df8ab4e3064e59c846fa1d
blendonl <blendonluta@gmail.com>
2025-07-08 14:00:21 +0000

    fix migration

--- db/migrate/20250708114932_add_parent_entry_to_entries.rb
+++ db/migrate/20250708114932_add_parent_entry_to_entries.rb
@@ -1,5 +1,5 @@
 class AddParentEntryToEntries < ActiveRecord::Migration[7.2]
   def change
-     add_reference :entries, :parent_entry, null: true, foreign_key: { to_table: :entries }
+add_reference :entries, :parent_entry, null: true, type: :uuid, foreign_key: { to_table: :entries }
   end
 end
